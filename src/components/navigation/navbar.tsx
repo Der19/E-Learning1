69b5de87a15eaaf1e3aada3acc06ce37e688bf77
Der19 <idoucour9@gmail.com>
2025-12-11 00:22:45 +0000

    logo

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -51,10 +51,10 @@ export function Navbar() {
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
           <div className="flex items-center space-x-3">
-            <div className="relative bg-gradient-primary p-2 rounded-lg">
-              <div className="relative">
-                <GraduationCap className="h-5 w-5 text-white absolute -top-1 left-1/2 transform -translate-x-1/2" />
-                <BookOpen className="h-4 w-4 text-white/90 mt-2" />
+            <div className="relative bg-gradient-primary p-2.5 rounded-lg flex items-center justify-center">
+              <div className="relative flex flex-col items-center">
+                <GraduationCap className="h-5 w-5 text-white mb-0.5" />
+                <BookOpen className="h-3.5 w-3.5 text-white/90" />
               </div>
             </div>
             <div>
